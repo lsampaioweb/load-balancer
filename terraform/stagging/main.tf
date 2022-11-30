3d0fea6ba6eaa969c656d9df03781a822dd297a0
Luciano Sampaio <lsampaioweb@gmail.com>
2022-11-29 23:44:38 -0300

    Moved the local provisioner to the root module.

--- terraform/stagging/main.tf
+++ terraform/stagging/main.tf
@@ -1,16 +1,16 @@
 module "random-target-node" {
-  for_each = var.vm_instance
-
   source = "../modules/random-target-node"
+
+  for_each = var.vm_instance
 }
 
 module "proxmox-ubuntu-22-04" {
+  source = "../modules/proxmox-ubuntu-22-04"
+
   for_each = var.vm_instance
 
-  source = "../modules/proxmox-ubuntu-22-04"
-  clone  = "ubuntu-22-04-server-std-docker"
-
   target_node = "kvm-0${module.random-target-node[each.key].result}"
+  clone       = "ubuntu-22-04-server-std-docker"
   name        = "stg-load-balancer-${each.key}"
   onboot      = each.value.onboot
   vcpus       = each.value.vcpus
@@ -66,4 +66,18 @@ resource "local_file" "ansible_hosts" {
 
   directory_permission = "0644"
   file_permission      = "0644"
+
+  provisioner "local-exec" {
+    working_dir = local.path_ansible_scripts
+
+    command = "ansible-playbook provision.yml"
+  }
+
+  # provisioner "local-exec" {
+  #   when = destroy
+
+  #   working_dir = local.path_ansible_scripts
+
+  #   command = "ansible-playbook destroy.yml"
+  # }
 }
--- terraform/stagging/output.tf
+++ terraform/stagging/output.tf
@@ -1,7 +1,3 @@
-# output "chosen-target-node" {
-#   value = module.random-target-node
-# }
-
 output "proxmox-virtual-machine" {
   value = module.proxmox-ubuntu-22-04
 }
