e41d55718f513b945cd2a24d914da08f77cb60fd
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-22 15:13:04 -0300

    Fixed the name of the module.

--- terraform/production/output.tf
+++ terraform/production/output.tf
@@ -1,3 +1,3 @@
 output "proxmox-virtual-machine" {
-  value = module.proxmox-ubuntu-22-04
+  value = module.proxmox-vm
 }
--- terraform/stagging/main.tf
+++ terraform/stagging/main.tf
@@ -4,8 +4,8 @@ module "random-target-node" {
   for_each = var.vm_instance
 }
 
-module "proxmox-ubuntu-22-04" {
-  source = "../modules/proxmox-ubuntu-22-04"
+module "proxmox-vm" {
+  source = "../modules/proxmox-vm"
 
   for_each = var.vm_instance
 
@@ -25,38 +25,38 @@ resource "local_file" "ansible_hosts" {
       master_list = [
         for key, value in var.vm_instance :
         {
-          hostname    = module.proxmox-ubuntu-22-04[key].vm_name
-          public_ip   = module.proxmox-ubuntu-22-04[key].vm_ipv4
-          password_id = module.proxmox-ubuntu-22-04[key].vm_cloned_from
+          hostname    = module.proxmox-vm[key].vm_name
+          public_ip   = module.proxmox-vm[key].vm_ipv4
+          password_id = module.proxmox-vm[key].vm_cloned_from
 
           state          = value.state
           priority       = value.priority
-          unicast_src_ip = module.proxmox-ubuntu-22-04[key].vm_ipv4
+          unicast_src_ip = module.proxmox-vm[key].vm_ipv4
 
           unicast_peer_ip = join(",", [
             for key1, value1 in var.vm_instance :
-            module.proxmox-ubuntu-22-04[key1].vm_ipv4
-            if module.proxmox-ubuntu-22-04[key].vm_ipv4 !=
-            module.proxmox-ubuntu-22-04[key1].vm_ipv4
+            module.proxmox-vm[key1].vm_ipv4
+            if module.proxmox-vm[key].vm_ipv4 !=
+            module.proxmox-vm[key1].vm_ipv4
           ])
         } if value.state == "MASTER"
       ]
       backup_list = [
         for key, value in var.vm_instance :
         {
-          hostname    = module.proxmox-ubuntu-22-04[key].vm_name
-          public_ip   = module.proxmox-ubuntu-22-04[key].vm_ipv4
-          password_id = module.proxmox-ubuntu-22-04[key].vm_cloned_from
+          hostname    = module.proxmox-vm[key].vm_name
+          public_ip   = module.proxmox-vm[key].vm_ipv4
+          password_id = module.proxmox-vm[key].vm_cloned_from
 
           state          = value.state
           priority       = value.priority
-          unicast_src_ip = module.proxmox-ubuntu-22-04[key].vm_ipv4
+          unicast_src_ip = module.proxmox-vm[key].vm_ipv4
 
           unicast_peer_ip = join(",", [
             for key1, value1 in var.vm_instance :
-            module.proxmox-ubuntu-22-04[key1].vm_ipv4
-            if module.proxmox-ubuntu-22-04[key].vm_ipv4 !=
-            module.proxmox-ubuntu-22-04[key1].vm_ipv4
+            module.proxmox-vm[key1].vm_ipv4
+            if module.proxmox-vm[key].vm_ipv4 !=
+            module.proxmox-vm[key1].vm_ipv4
           ])
         } if value.state == "BACKUP"
       ]
