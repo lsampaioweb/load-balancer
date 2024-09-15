8ad6b469a7e5706d28ab02240876f3e1b3416737
Luciano Sampaio <lsampaioweb@gmail.com>
2024-09-15 11:01:03 -0300

    feat: Update the output to show the url address based on the environment.

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,10 +5,20 @@ output "virtual_machines" {
 output "app_urls" {
   description = "URLs of the application for each VM, including the base URL"
   value = concat(
-    ["https://loadbalancer.lan.homelab"], # Fixed base URL
+    [
+      # Use conditional expression to prepend "edge-" only for Staging.
+      format(
+        "https://%sloadbalancer.lan.homelab",
+        var.environment == "Staging" ? "edge-" : ""
+      )
+    ],
     [
       for key in keys(module.homelab_project.vms) :
-      format("https://loadbalancer-%s.lan.homelab", key)
+      format(
+        "https://%sloadbalancer-%s.lan.homelab",
+        var.environment == "Staging" ? "edge-" : "",
+        key
+      )
     ]
   )
 }
